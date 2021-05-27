9c18f89fb4ea9d3182f1c696fe8ef4195e471c0d
Manuel Sarmiento Martinez <manuel.sarmiento@experis.es>
2021-05-27 18:05:40 +0200

    Added tests

--- curso/request_name.py
+++ curso/request_name.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import unittest
 from lxml import html
 
 print("Nombre del cual deseas conocer el género:")
@@ -12,9 +13,18 @@ print(r.url)
 
 tree = html.fromstring(r.content)
 gender = tree.xpath('//a[@title="Identidad sexual"]/text()')
-namegender = tree.xpath('//div[@class="mw-parser-output"]//table//tbody//td//text()')
+array_gender = tree.xpath('//div[@class="mw-parser-output"]//table//tbody//td//text()')
 footer = tree.xpath('//div[@class="action-list"]//p/text()')
 
-for item in namegender:
+for item in array_gender:
     if item in ["\nMasculino", "\nFemenino"]:
-        print("Género: " + item)
+        name_gender = item
+        break
+
+print("Género: " + name_gender)
+
+
+class TestResult(unittest.TestCase):
+
+    def test_gender(self):
+        self.assertIs(len(name_gender) > 0)
