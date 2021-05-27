d8dd589a742dc9c0823d865fdf0563316600bfe4
Manuel Sarmiento Martinez <manuel.sarmiento@experis.es>
2021-05-27 18:39:09 +0200

    Deleted pycache

--- curso/request_name.py
+++ curso/request_name.py
@@ -5,26 +5,26 @@ import requests
 import unittest
 from lxml import html
 
-print("Nombre del cual deseas conocer el género:")
-nombre = input()
-r = requests.get(f'https://es.wikipedia.org/wiki/{nombre}_(nombre)')
-
-print(r.url)
-
-tree = html.fromstring(r.content)
-gender = tree.xpath('//a[@title="Identidad sexual"]/text()')
-array_gender = tree.xpath('//div[@class="mw-parser-output"]//table//tbody//td//text()')
-footer = tree.xpath('//div[@class="action-list"]//p/text()')
-
-for item in array_gender:
-    if item in ["\nMasculino", "\nFemenino"]:
-        name_gender = item
-        break
-
-print("Género: " + name_gender)
-
 
 class TestResult(unittest.TestCase):
 
     def test_gender(self):
-        self.assertIs(len(name_gender) > 0)
+        print("Nombre del cual deseas conocer el género:")
+        nombre = input()
+        r = requests.get(f'https://es.wikipedia.org/wiki/{nombre}_(nombre)')
+
+        print(r.url)
+
+        tree = html.fromstring(r.content)
+        gender = tree.xpath('//a[@title="Identidad sexual"]/text()')
+        array_gender = tree.xpath('//div[@class="mw-parser-output"]//table//tbody//td//text()')
+        footer = tree.xpath('//div[@class="action-list"]//p/text()')
+
+        for item in array_gender:
+            if item in ["\nMasculino", "\nFemenino"]:
+                name_gender = item
+                break
+
+        print("Género: " + name_gender)
+        
+        self.assertTrue(len(name_gender) > 0)
